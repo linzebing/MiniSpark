8e8328575e20e667d3c743792a0a6f8ac63df23d
Zebing Lin <linzebing1995@gmail.com>
2017-04-16 21:19:10 -0400

    Test Hadoop

--- src/main/java/minispark/App.java
+++ src/main/java/minispark/App.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class App {
   public static void main(String[] args) throws IOException {
     SparkContext sc = new SparkContext("Example");
-    Rdd lines = sc.textFile("hdfs://ec2-34-200-235-149.compute-1.amazonaws.com:54310/test.txt");
+    Rdd lines = sc.textFile("hdfs:///test.txt");
     List<String> output = lines.collect();
     for (String line: output) {
       System.out.println(line);
