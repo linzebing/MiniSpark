a4b313d678cafe8105140cc7dc97a2b7710eff36
Zebing Lin <linzebing1995@gmail.com>
2017-04-29 23:03:15 -0400

    Can run a thrift demo now.

--- src/main/java/minispark/Common.java
+++ src/main/java/minispark/Common.java
@@ -14,4 +14,18 @@ public class Common {
   public enum OperationType {
     Map, FlatMap, Reduce, ReduceByKey, Filter, Collect, HdfsFile
   }
+
+  /*
+  class DoJobArgs {
+    public OperationType operationType;
+    public String hdfsFile;
+    public int hdfsSplitId;
+
+    public DoJobArgs(OperationType _operationType, String _hdfsFile, int _hdfsSplitId) {
+      this.operationType = _operationType;
+      this.hdfsFile = _hdfsFile;
+      this.hdfsSplitId = _hdfsSplitId;
+    }
+  }
+  */
 }
