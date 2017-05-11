bc030365958e131b1f9c76137d0e2485fcb3239d
Zebing Lin <linzebing1995@gmail.com>
2017-05-11 13:48:25 -0400

    Test correctness of dfs

--- src/main/java/minispark/WorkerServiceHandler.java
+++ src/main/java/minispark/WorkerServiceHandler.java
@@ -335,6 +335,7 @@ public class WorkerServiceHandler implements WorkerService.Iface {
           } catch (Exception e) {
             e.printStackTrace();
           }
+          break;
       }
     }
   }
