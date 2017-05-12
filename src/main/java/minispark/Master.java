cedca37a6ea3e8ea9bd01f36c92af483f2cf0edd
Zebing Lin <linzebing1995@gmail.com>
2017-05-12 13:52:02 -0400

    workers = 6

--- src/main/java/minispark/Master.java
+++ src/main/java/minispark/Master.java
@@ -26,7 +26,7 @@ public class Master {
   HashMap<WorkerService.Client, Boolean> availableMap;
   HashMap<String, Integer> countsMap;
   public static final int numClientsPerWorker = 4;
-  public static final int numWorkers = 2;
+  public static final int numWorkers = 6;
   public static final int sleepTime = 100;
   public static final Object lock = new Object();
 
