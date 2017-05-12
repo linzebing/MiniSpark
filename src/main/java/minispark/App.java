875efccf27d1a5228db45f0a61fca253786d6fb0
Zebing Lin <linzebing1995@gmail.com>
2017-05-12 09:34:42 -0400

    add wc

--- src/main/java/minispark/App.java
+++ src/main/java/minispark/App.java
@@ -26,6 +26,36 @@ public class App {
     return result;
   }
 
+  public static String mapTest(String s) {
+    return s.toLowerCase();
+  }
+
+  public static ArrayList<String> flatMapTest(String s) {
+    return new ArrayList<String>(Arrays.asList(s.split(" ")));
+  }
+
+  public static double reduceByKeyTest(double a, double b) {
+    return a + b;
+  }
+
+  public static boolean InstagramOnly(String s) {
+    return s.endsWith("15618") || s.startsWith("15618");
+  }
+
+  public static void wc() throws IOException, TException {
+    SparkContext sc = new SparkContext("Example");
+    Rdd lines = sc.textFile("webhdfs://ec2-54-208-160-33.compute-1.amazonaws.com/test.txt").flatMap("flatMapTest")
+        .map("mapTest").filter("InstagramOnly").mapPair("mapCount").reduceByKey("reduceByKeyTest");
+    Long start = System.currentTimeMillis();
+    List<StringNumPair> output = (List<StringNumPair>) lines.collect();
+    for (StringNumPair pair: output) {
+      System.out.println(pair.str + " " + pair.num);
+    }
+    Long end = System.currentTimeMillis();
+    System.out.println("Time elapsed: " + (end - start) / 1000 + " seconds");
+    sc.stop();
+  }
+
   public static double add(double a, double b) {
     return a + b;
   }
@@ -85,6 +115,7 @@ public class App {
 
   public static void main(String[] args) throws IOException, TException {
     //SparkPi();
-    Histogram();
+    //Histogram();
+    wc();
   }
 }
